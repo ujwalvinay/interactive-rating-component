889ecb5960bd04d4d69eb2d525868e08250509ff
Ujwal Vinay <walkerdoodle7@gmail.com>
2022-11-05 23:26:50 +0530

    main.js added

---
+++ main.js
@@ -0,0 +1,33 @@
+const ratingDiv = $(".ratingDiv");
+const thankDiv = $(".thankDiv");
+
+const ratingBtn = $(".rating-btn");
+const submitBtn = $(".submit-btn");
+const rateValue = $("#rateValue");
+
+let rating;
+
+
+ratingBtn.click(function(e){
+    if($(this).hasClass("ratinButtonPressed"))
+    {
+        $(ratingBtn).removeClass("ratingButtonPressed");
+        $(this).addClass("ratinButtonDePressed");
+    }
+    else
+    {
+        $(this).removeClass("ratingDeButtonPressed");
+        $(this).addClass("ratinButtonPressed");
+    }
+    rating = e.target.id;
+    
+})
+
+
+submitBtn.click(function()
+{
+    rateValue.text(""+rating);    
+    ratingDiv.hide();
+    thankDiv.css("display","block");
+
+})
